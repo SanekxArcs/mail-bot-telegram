889f5158fae8128572be98700e22ea59a539bc77
Oleksandr Dzisiak <sanek.arc@gmail.com>
2024-10-23 23:26:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// app.js
+
 require("dotenv").config();
 
 const telegramController = require("./controllers/telegramController");
@@ -7,9 +9,37 @@ const logger = require("./utils/logger");
 // Запускаємо перевірку пошти кожні 5 хвилин
 setInterval(() => {
   emailController.checkEmail();
-},  10 * 60 * 1000);
+}, 5 * 60 * 1000); // 5 хвилин
+
+// Запускаємо щоденне надсилання підсумку о 18:00
+scheduleDailySummary(18, 0); // Час у 24-годинному форматі: 18:00
 
 // Запускаємо Telegram бота
 telegramController.startBot();
 
 logger.info("Бот запущено та готовий до роботи.");
+
+// Функція для планування щоденного підсумку
+function scheduleDailySummary(hour, minute) {
+  const now = new Date();
+  const nextTime = new Date();
+
+  nextTime.setHours(hour);
+  nextTime.setMinutes(minute);
+  nextTime.setSeconds(0);
+
+  if (now > nextTime) {
+    // Якщо час уже пройшов, плануємо на наступний день
+    nextTime.setDate(nextTime.getDate() + 1);
+  }
+
+  const timeUntilNext = nextTime - now;
+
+  setTimeout(() => {
+    emailController.sendDailySummary();
+    // Після виконання плануємо наступне виконання через 24 години
+    setInterval(() => {
+      emailController.sendDailySummary();
+    }, 24 * 60 * 60 * 1000); // 24 години
+  }, timeUntilNext);
+}
